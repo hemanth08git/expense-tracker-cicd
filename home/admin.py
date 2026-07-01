a1c7ca100ea22b6060bd310d2bb53981cb351fd6
EC2 Default User <ec2-user@ip-172-31-15-7.ec2.internal>
2026-07-01 05:50:57 +0000

    changes

--- home/admin.py
+++ home/admin.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
-# Register your models here.
 from .models import Addmoney_info
-
 class Addmoney_infoAdmin(admin.ModelAdmin):
     list_display=("user","quantity","Date","Category","add_money")
 admin.site.register(Addmoney_info,Addmoney_infoAdmin)
